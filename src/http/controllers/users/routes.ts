5d477b80d4bb5a4c59ebc94a441b7b9dce71e3b2
Samuel Belo <belo.samuel@gmail.com>
2024-09-26 06:41:30 -0300

    refresh token strategy added

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,11 +2,14 @@ import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { authenticateController } from "./authenticate.controller";
 import { profileController } from "./profile.controller";
+import { refreshController } from "./refresh.controller";
 import { registerController } from "./register.controller";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/users', registerController)
     app.post('/auth', authenticateController)
+
+    app.patch('/token/refresh', refreshController)
 
     app.get('/profile', { onRequest: [verifyJWT] }, profileController)
 }
